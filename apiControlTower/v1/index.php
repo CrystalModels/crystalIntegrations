71541310255ee0055c24558f2b0c3f274d508e1b
Juan <juan.paez@api-ux.com>
2023-07-06 19:09:27 -0500

    Nueva funcionalidad

--- apiControlTower/v1/index.php
+++ apiControlTower/v1/index.php
@@ -187,59 +187,15 @@ Flight::route('POST /postRooms/@headerslink', function ($headerslink) {
 });
 
 
-Flight::route('POST /postRooms1/@headerslink', function ($headerslink) {
+Flight::route('POST /postRooms1/@number', function ($number) {
 
     header("Access-Control-Allow-Origin: *");
-    $parts = explode(" ", $headerslink);
-
-    $apiKey=$parts[0];
-    $xApiKey=$parts[1];
+    
     // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apiKey) && !empty($xApiKey)) {    
-            
-           
-       
-
-
-
-
-
-
-        
-
-
-
-
-        $sub_domaincon=new model_domain();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKey/';
-      
-        $data = array(
-          'apiKey' =>$apiKey, 
-          'xApiKey' => $xApiKey
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-
-
-      curl_close($curl);
-echo "si si";
-      
-    } else {
-        echo 'Error: Encabezados faltantes';
-    }
+    
+echo "si si ";
+      echo $number;
+    
 });
 
 
