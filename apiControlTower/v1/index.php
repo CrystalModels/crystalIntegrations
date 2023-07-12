45d056646e0828f03e7848c9202886ead103d726
Juan <juan.paez@api-ux.com>
2023-07-12 15:40:45 -0500

    Nueva funcionalidad

--- apiControlTower/v1/index.php
+++ apiControlTower/v1/index.php
@@ -219,7 +219,75 @@ Flight::route('POST /putRooms/@apk/@xapk', function ($apk,$xapk) {
     $query2= mysqli_query($conectar,"UPDATE rooms SET name='$name',comments='$comments' where roomId='$roomId'");
                
                          
- echo "true¡Room editado con exito!";
+ echo "true*¡Room editado con exito!";
+
+
+           // echo json_encode($response1);
+        } else {
+            echo 'false*¡Autenticación fallida!';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'false*¡Encabezados faltantes!';
+    }
+});
+
+
+Flight::route('POST /putMyAlert/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+            
+        // Leer los datos de la solicitud
+        
+            
+
+
+
+
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->dom();
+        $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKey/';
+      
+        $data = array(
+            'apiKey' =>$apk, 
+            'xApiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+
+            $response= Flight::request()->data->response;
+            $alertId= Flight::request()->data->alertId;
+
+    $conectar=conn();
+    
+    $query2= mysqli_query($conectar,"UPDATE generalAlerts SET alertResponse='$response',isActive=0 where alertId='$alertId'");
+               
+                         
+ echo "true*¡Alerta editada con exito!";
 
 
            // echo json_encode($response1);
