ca3a00836b12249256452f4bec2d2b91e114aefa
Juan <juan.paez@api-ux.com>
2023-07-12 17:36:48 -0500

    Nueva funcionalidad

--- apiControlTower/v1/index.php
+++ apiControlTower/v1/index.php
@@ -281,13 +281,87 @@ Flight::route('POST /putMyAlert/@apk/@xapk', function ($apk,$xapk) {
 
             $response= Flight::request()->data->response;
             $alertId= Flight::request()->data->alertId;
+            $profileId= Flight::request()->data->profileId;
 
     $conectar=conn();
     
-    $query2= mysqli_query($conectar,"UPDATE generalAlerts SET alertResponse='$response',isActive=0 where alertId='$alertId'");
+    $query2= mysqli_query($conectar,"UPDATE generalAlerts SET alertResponse='$response',isActive=0 where alertId='$alertId' and profileId='$profileId'");
                
                          
  echo "true*¡Alerta editada con exito!";
+
+
+           // echo json_encode($response1);
+        } else {
+            echo 'false*¡Autenticación fallida!';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'false*¡Encabezados faltantes!';
+    }
+});
+
+
+Flight::route('POST /postAlert/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+            
+        // Leer los datos de la solicitud
+        
+            
+
+
+
+
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->dom();
+        $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKey/';
+      
+        $data = array(
+            'apiKey' =>$apk, 
+            'xApiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+
+            $comments= Flight::request()->data->comments;
+            $ownerId= Flight::request()->data->ownerId;
+            $profileId= Flight::request()->data->profileId;
+
+    $conectar=conn();
+    
+    require_once '../../apiControlTower/v1/model/modelSecurity/uuid/uuidd.php';
+    $gen_uuid = new generateUuid();
+    $myuuid = $gen_uuid->guidv4();
+    $primeros_ocho = substr($myuuid, 0, 8);
+    $query2= mysqli_query($conectar,"INSERT INTO generalAlerts (alertId,comments,profileId,ownerId,alertType) VALUES ('$primeros_ocho','$comments','$profileId','$ownerId','ALERT')");
+               
+                         
+ echo "true*¡Alerta enviada con exito!";
 
 
            // echo json_encode($response1);
