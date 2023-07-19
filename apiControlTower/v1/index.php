21d3a76963d14b8c82211fd203acc9b79e2cf848
Juan <juan.paez@api-ux.com>
2023-07-18 20:45:14 -0500

    Nueva funcionalidad

--- apiControlTower/v1/index.php
+++ apiControlTower/v1/index.php
@@ -2153,7 +2153,7 @@ Flight::route('GET /getAllPagesModels/@modelId', function ($modelId) {
             $conectar=conn();
             
           
-            $query= mysqli_query($conectar,"SELECT tl.transId,tl.profileId,tl.status,tl.isActive,tl.pageId,tl.valueNow,pl.name as pageName,pl.urlPage,tr.startTime,tr.endTime,tr.startDate,tr.transId as trId,tr.endDate FROM pageAssignation tl JOIN generalPages pl ON pl.pageId=tl.pageId JOIN transmissionRecord tr ON tr.pageId=pl.pageId where tl.profileId='$modelId' and tl.valueNow='false' and tr.isActive=1 ");
+            $query= mysqli_query($conectar,"SELECT tl.transId,tl.profileId,tl.status,tl.isActive,tl.pageId,tl.valueNow,pl.name as pageName,pl.urlPage,tr.startTime,tr.endTime,tr.startDate,tr.transId as trId,tr.endDate FROM pageAssignation tl JOIN generalPages pl ON pl.pageId=tl.pageId JOIN transmissionRecord tr ON tr.profileId=pl.profileId where tl.profileId='$modelId' and tl.valueNow='false' and tr.isActive=1 ");
                
           
                 $values=[];
