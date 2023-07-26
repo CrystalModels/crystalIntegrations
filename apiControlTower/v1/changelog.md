c9f40d4d10db863604ecd0dd9e756629908074c9
Juan <juan.paez@api-ux.com>
2023-07-26 14:30:10 -0500

    visualizador de versiones v1.0.5-beta

--- apiControlTower/v1/changelog.md
+++ apiControlTower/v1/changelog.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## [1.0.1-Beta] - 2023-07-26
+- Sistema de versionamiento
 ## [1.0.0-Beta] - 2023-07-25
 - Sistema base
 
--- apiControlTower/v1/versions/versions.php
+++ apiControlTower/v1/versions/versions.php
@@ -7,6 +7,7 @@ function ver_change() {
     $values=[];
 
     $value=[
+        '1.0.1-Beta' => '2023-07-26-sistema de versionamiento',
         '1.0.0-Beta' => '2023-07-25-sistema base'
         
     ];
