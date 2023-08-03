a27de70f27df3e6fc119d23ba5c893d66876968f
Juan <juan.paez@api-ux.com>
2023-08-02 21:28:12 -0500

    visualizador de versiones v1.0.5-beta

--- apiControlTower/v1/index.php
+++ apiControlTower/v1/index.php
@@ -4867,6 +4867,79 @@ Flight::route('POST /postCutting/@apk/@xapk', function ($apk,$xapk) {
 
 
 
+
+
+
+Flight::route('POST /adjustModelEarn/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+            
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->dom();
+        $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKey/';
+      
+        $data = array(
+          'apiKey' =>$apk, 
+          'xApiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+
+         
+            $value= Flight::request()->data->value;
+            $earnId= Flight::request()->data->earnId;
+            $comment= Flight::request()->data->comment;
+            
+
+    $conectar=conn();
+   
+    $query2= mysqli_query($conectar,"UPDATE modelEarn SET '$value'='$comment' where earnId='$earnId'");
+               
+    if ($query2) {
+        echo "true*¡Ajustado con exito!";
+    } else {
+        echo "false*¡Error en la consulta! " . mysqli_error($conectar);
+    }        
+ 
+
+          
+           // echo json_encode($response1);
+        } else {
+            echo 'false*¡Autenticación fallida!';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'false*¡Encabezados faltantes!';
+    }
+});
+
+
+
 Flight::route('POST /putCuttingStatus/@apk/@xapk', function ($apk,$xapk) {
   
     header("Access-Control-Allow-Origin: *");
