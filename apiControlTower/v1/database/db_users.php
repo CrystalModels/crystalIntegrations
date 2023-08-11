f324aba1e5457f9e99669430f86c67158cc156a0
Juan <juan.paez@api-ux.com>
2023-08-11 09:48:08 -0500

    sistemas base v1.0.0

--- apiControlTower/v1/database/db_users.php
+++ apiControlTower/v1/database/db_users.php
@@ -78,9 +78,9 @@ if($option==3){
 }
 if($option==4){
     $hostname = "localhost"; // o dirección IP del servidor de la base de datos remota
-    $usuariodb = "u495616274_crystalint"; // reemplazar con el usuario de la base de datos
+    $usuariodb = "u495616274_crystalinteg"; // reemplazar con el usuario de la base de datos
     $passworddb = "e8:WTU8jAzY"; // reemplazar con la contraseña de la base de datos
-    $dbname = "u495616274_crystalint"; // reemplazar con el nombre de la base de datos
+    $dbname = "u495616274_crystalinteg"; // reemplazar con el nombre de la base de datos
 
     try {
         // Conexión a la base de datos
